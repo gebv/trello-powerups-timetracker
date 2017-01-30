f694ae232486066f4b760b67cae57bf4a462a59f
G B <egor.workroom@gmail.com>
2017-01-30 21:07:19 +0600

    test badges

--- js/template.js
+++ js/template.js
@@ -1,4 +1,51 @@
 
+var getBadges = function(t){
+  return t.card('name')
+  .get('name')
+  .then(function(cardName){
+    var badgeColor;
+    // var icon = GRAY_ICON;
+    var lowercaseName = cardName.toLowerCase();
+    if(lowercaseName.indexOf('green') > -1){
+      badgeColor = 'green';
+    //   icon = WHITE_ICON;
+    } else if(lowercaseName.indexOf('yellow') > -1){
+      badgeColor = 'yellow';
+    //   icon = WHITE_ICON;
+    } else if(lowercaseName.indexOf('red') > -1){
+      badgeColor = 'red';
+    //   icon = WHITE_ICON;
+    }
+
+    if(lowercaseName.indexOf('dynamic') > -1){
+      // dynamic badges can have their function rerun after a set number
+      // of seconds defined by refresh. Minimum of 10 seconds.
+      return [{
+        dynamic: function(){
+          return {
+            title: 'Detail Badge', // for detail badges only
+            text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
+            // icon: icon, // for card front badges only
+            color: badgeColor,
+            refresh: 10
+          }
+        }
+      }]
+    }
+
+    if(lowercaseName.indexOf('static') > -1){
+      // return an array of badge objects
+      return [{
+        title: 'Detail Badge', // for detail badges only
+        text: 'Static',
+        // icon: icon, // for card front badges only
+        color: badgeColor
+      }];
+    } else {
+      return [];
+    }
+  })
+};
 
 TrelloPowerUp.initialize({
     'show-settings': function (t, options) {
@@ -8,16 +55,7 @@ TrelloPowerUp.initialize({
             height: 184
         });
     },
-    'card-badges': function (t, card) {
-        return {
-            dynamic: function () {
-                return {
-                    title: 'Detail Badge', // for detail badges only
-                    text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
-                    color: 'green',
-                    refresh: 10
-                }
-            }
-        }
+    'card-badges': function(t, options) {
+        return getBadges(t);
     }
 })
