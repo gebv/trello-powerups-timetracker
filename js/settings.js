4c060ca1d4baed8da54b799c1b1d45a6fabace6d
G B <egor.workroom@gmail.com>
2017-01-30 20:57:32 +0600

    добавление информации карточкам

--- js/settings.js
+++ js/settings.js
@@ -4,31 +4,28 @@ var t = TrelloPowerUp.iframe();
 var doneListName = document.getElementById('SettingsField-NameListOfDone');
 var progressListName = document.getElementById('SettingsField-NameListOfInProgress');
 
-t.render(function(){
-  return Promise.all([
-    t.get('board', 'shared', 'doneListName'),
-    t.get('board', 'shared', 'progressListName')
-  ])
-  .spread(function(doneListName, progressListName){
-    if(doneListName && /[a-zA-Z0-9._ ]+/.test(doneListName)){
-      doneListName.value = doneListName;
-    }
-    if(progressListName && /[a-z]+/.test(progressListName)){
-      progressListName.value = progressListName;
-    }
-  })
-  .then(function(){
-    t.sizeTo('#content')
-    .done();
-  })
+t.render(function () {
+    return Promise.all([
+        t.get('board', 'shared', 'doneListName'),
+        t.get('board', 'shared', 'progressListName')
+    ])
+        .spread(function (doneListName, progressListName) {
+            // TODO: валидация
+            doneListName.value = doneListName;
+            progressListName.value = progressListName;
+        })
+        .then(function () {
+            t.sizeTo('#content')
+                .done();
+        })
 });
 
-document.getElementById('settings-btn-save').addEventListener('click', function(){
-  return t.set('board', 'shared', 'doneListName', doneListName.value)
-  .then(function(){
-    return t.set('board', 'shared', 'progressListName', progressListName.value);
-  })
-  .then(function(){
-    t.closePopup();
-  })
+document.getElementById('settings-btn-save').addEventListener('click', function () {
+    return t.set('board', 'shared', 'doneListName', doneListName.value)
+        .then(function () {
+            return t.set('board', 'shared', 'progressListName', progressListName.value);
+        })
+        .then(function () {
+            t.closePopup();
+        })
 })
--- js/template.js
+++ js/template.js
@@ -1,11 +1,23 @@
 
 
 TrelloPowerUp.initialize({
-    'show-settings': function(t, options){
-    return t.popup({
-      title: 'Settings',
-      url: './settings.html',
-      height: 184
-    });
-  }
+    'show-settings': function (t, options) {
+        return t.popup({
+            title: 'Settings',
+            url: './settings.html',
+            height: 184
+        });
+    },
+    'card-badges': function (t, card) {
+        return {
+            dynamic: function () {
+                return {
+                    title: 'Detail Badge', // for detail badges only
+                    text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
+                    color: 'green',
+                    refresh: 10
+                }
+            }
+        }
+    }
 })
